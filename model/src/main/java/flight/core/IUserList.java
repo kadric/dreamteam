d0e2e397bbdeee9a627e6ead7412a44908fc59c8
Dženan <Denan@213.112.165.193>
2014-09-27 19:03:14 +0200

    + UserList, IUserList

---
+++ model/src/main/java/flight/core/IUserList.java
@@ -0,0 +1,16 @@
+
+package flight.core;
+
+import flight.util.IDAO;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ * Interface to product catalogue
+ * @author hajo
+ */
+@Local
+public interface IUserList extends IDAO<User, Long> {
+
+     
+}
