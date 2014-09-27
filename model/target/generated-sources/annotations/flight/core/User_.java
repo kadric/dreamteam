4eb8fd5912803c4d2b53a81f25e35ec1572c4021
Dz <DzenoBa@users.noreply.github.com>
2014-09-27 19:06:55 +0200

    Why do I commit with user Dzenan not DzenoBa?

---
+++ model/target/generated-sources/annotations/flight/core/User_.java
@@ -0,0 +1,17 @@
+package flight.core;
+
+import flight.core.Groups;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-27T18:36:57")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> email;
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile ListAttribute<User, Groups> groups;
+
+}
