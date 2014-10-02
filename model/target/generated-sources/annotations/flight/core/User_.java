5e78f67f09931619a5afff6c71badbd717f9106d
Dz <DzenoBa@users.noreply.github.com>
2014-10-02 19:15:35 +0200

    Model->jar, added model as depend in front

--- model/target/generated-sources/annotations/flight/core/User_.java
+++ model/target/generated-sources/annotations/flight/core/User_.java
@@ -1,14 +1,17 @@
 package flight.core;
 
+import flight.core.Groups;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-27T19:46:33")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-02T19:09:26")
 @StaticMetamodel(User.class)
 public class User_ { 
 
     public static volatile SingularAttribute<User, String> email;
     public static volatile SingularAttribute<User, String> password;
+    public static volatile ListAttribute<User, Groups> groups;
 
 }
