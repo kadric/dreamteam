523f6ab3a2e79815079ba5e14a81afc41908c001
kadric <kadric@student.chalmers.se>
2014-10-13 15:15:50 +0200

    added ICustomerList interface class

---
+++ backend/src/main/java/edu/cth/dzenkyair/backend/core/ICustomerList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cth.dzenkyair.backend.core;
+
+import edu.cth.dzenkyair.backend.util.IDAO;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author matej
+ * 
+ */
+@Local
+public interface ICustomerList extends IDAO<Customer, Long> {
+        
+        public List<Customer> getByUser(Long id);
+}
