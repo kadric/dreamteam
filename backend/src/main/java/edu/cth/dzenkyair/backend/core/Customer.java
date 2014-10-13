077b79b3328dbb87a5ea8bb8551dec1ab1ada761
kadric <kadric@student.chalmers.se>
2014-10-13 15:12:49 +0200

    added Customer class

---
+++ backend/src/main/java/edu/cth/dzenkyair/backend/core/Customer.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cth.dzenkyair.backend.core;
+
+import edu.cth.dzenkyair.backend.util.AbstractEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author hagakure
+ */
+@Entity
+@Table(name="CUSTOMERS")
+public class Customer extends AbstractEntity{
+    
+    @ManyToOne
+    private User user;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String adress;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private Long phone;
+    
+    
+    public Customer(){
+        ;
+    }
+    
+    public Customer(User user, String firstName, String lastName,
+                        String adress, String city, Long phone){
+        this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.city = city;
+        this.phone = phone;
+    
+    }
+    
+    public Customer(Long id, User user, String firstName, String lastName,
+                        String adress, String city, Long phone){
+        super(id);
+        this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.city = city;
+        this.phone = phone;
+    
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+    
+    @Override
+    public String toString(){
+    return "Customer{" + "Id=" + getId() + ", User=" + user.getId()
+                + ", First name =" + firstName
+                + ", Last name =" + lastName
+                + ", Adress =" + adress
+                + ", City =" + city
+                + ", Phone number =" + phone
+                + '}';
+    }
+    
+}
