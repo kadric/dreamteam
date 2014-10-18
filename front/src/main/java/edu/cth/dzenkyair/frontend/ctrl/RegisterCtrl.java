b46c8543867f3c5695542b344857d066d2b4532f
kadric <kadric@student.chalmers.se>
2014-10-18 21:05:44 +0200

    created register.xhtml and supporting ctrl and bb. added all sites in to navigation-rule in faces-config.xml

---
+++ front/src/main/java/edu/cth/dzenkyair/frontend/ctrl/RegisterCtrl.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cth.dzenkyair.frontend.ctrl;
+
+import edu.cth.dzenkyair.backend.core.Customer;
+import edu.cth.dzenkyair.backend.core.FlightModel;
+import edu.cth.dzenkyair.backend.core.Groups;
+import edu.cth.dzenkyair.backend.core.User;
+import edu.cth.dzenkyair.frontend.view.RegisterBB;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author hagakure
+ */
+@Named
+@RequestScoped
+public class RegisterCtrl implements Serializable{
+    
+    @Inject
+    private FlightModel flightModel;
+    
+    private RegisterBB registerBB;
+        
+    FacesContext context = FacesContext.getCurrentInstance();
+    ExternalContext externalContext = context.getExternalContext();
+    
+    
+    protected RegisterCtrl(){
+    
+    }
+    
+    @Inject
+    public void setRegisterBB(RegisterBB registerBB) {
+        this.registerBB = registerBB;
+    }
+    
+    public Collection<Customer> getCustomerList(){
+        //List<Customer> cl = (List<Customer>) externalContext.getSessionMap().get("customerList");
+        List<Customer>cl = flightModel.getCustomerList().findAll();
+        return cl;
+    }
+    
+    public String register(){
+   
+        if(registerBB.getFirstName().isEmpty() || registerBB.getLastName().isEmpty() 
+                || registerBB.getPassword().isEmpty() || registerBB.getConfirmPassword().isEmpty()
+                || registerBB.getEmail().isEmpty() || registerBB.getAdress().isEmpty()
+                || registerBB.getCity().isEmpty() || registerBB.getPhone() == null){
+            
+                registerBB.setError("Please type in all fields");
+                return "register?faces-redirect=false";
+        }
+        
+        else if( !(registerBB.getPassword()).equals(registerBB.getConfirmPassword())){
+        
+                registerBB.setError("Make sure that password is correct");
+                return "register?faces-redirect=false";
+        
+        }else if( flightModel.getUserList().getByEmail(registerBB.getEmail()).size() > 0  ){
+                
+                registerBB.setError("User exists");
+                return "register?faces-redirect=false";
+        
+        }else {
+            
+            
+                List<Customer>cl = flightModel.getCustomerList().findAll();
+                User u = new User(registerBB.getEmail(),registerBB.getPassword(),Groups.USER);
+                Customer c = new Customer(u,registerBB.getFirstName(),registerBB.getLastName(),
+                                        registerBB.getAdress(),registerBB.getCity(),registerBB.getPhone());
+                    
+                flightModel.getUserList().create(u);
+                
+                flightModel.getCustomerList().create(c);
+            
+            
+            }
+        return "login?faces-redirect=true";
+    }
+      
+    
+   
+}
---
+++ front/src/main/java/edu/cth/dzenkyair/frontend/view/RegisterBB.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cth.dzenkyair.frontend.view;
+
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author hagakure
+ */
+@Named
+@RequestScoped
+public class RegisterBB implements Serializable{
+
+    private String firstName;
+    private String lastName;
+  
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private String adress;
+    private String city;
+    private Long   phone;
+    private String error;
+
+    
+    public RegisterBB() {
+        ;
+    }
+    
+    
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+    
+    
+    
+}
