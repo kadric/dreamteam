561b44d141e28b5e2866c54861fabc585c7042ae
Dz <DzenoBa@users.noreply.github.com>
2014-10-22 15:27:25 +0200

    + viewOrderBB

---
+++ front/src/main/java/edu/cth/dzenkyair/frontend/view/ViewOrderBB.java
@@ -0,0 +1,50 @@
+
+package edu.cth.dzenkyair.frontend.view;
+
+import edu.cth.dzenkyair.backend.core.Flight;
+import edu.cth.dzenkyair.backend.core.FlightModel;
+import edu.cth.dzenkyair.backend.core.Order;
+import edu.cth.dzenkyair.backend.core.Passenger;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Dženan
+ */
+@Named
+@RequestScoped
+public class ViewOrderBB implements Serializable {
+    
+    private static final Logger LOG = Logger.getLogger(ViewOrderBB.class.getName());
+    
+    @Inject // Bad use setter or constructor injection
+    private FlightModel flightModel;
+    
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Flight getFlight() {
+        Order o = flightModel.getOrderList().find(id);
+        Flight f = o.getOrderFlight();
+        return f;
+    }
+    
+    public List<Passenger> getPassengers() {
+        Order o = flightModel.getOrderList().find(id);
+        List<Passenger> ps = flightModel.getPassengerList().getByOrder(o);
+        return ps;
+    }
+}
