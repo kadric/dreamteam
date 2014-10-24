a8047b854cac6f27181d20c8b9fb7a4fb682392c
Dz <DzenoBa@users.noreply.github.com>
2014-10-24 19:03:11 +0200

    some fixes in viewOrder

--- front/src/main/java/edu/cth/dzenkyair/frontend/view/ViewOrderBB.java
+++ front/src/main/java/edu/cth/dzenkyair/frontend/view/ViewOrderBB.java
@@ -3,13 +3,18 @@ package edu.cth.dzenkyair.frontend.view;
 
 import edu.cth.dzenkyair.backend.core.Flight;
 import edu.cth.dzenkyair.backend.core.FlightModel;
+import edu.cth.dzenkyair.backend.core.Groups;
 import edu.cth.dzenkyair.backend.core.Order;
 import edu.cth.dzenkyair.backend.core.Passenger;
+import edu.cth.dzenkyair.backend.core.User;
+import edu.cth.dzenkyair.frontend.session.FlightSession;
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -18,7 +23,7 @@ import javax.inject.Named;
  * @author Dženan
  */
 @Named
-@RequestScoped
+@ViewScoped
 public class ViewOrderBB implements Serializable {
     
     private static final Logger LOG = Logger.getLogger(ViewOrderBB.class.getName());
@@ -26,12 +31,14 @@ public class ViewOrderBB implements Serializable {
     @Inject // Bad use setter or constructor injection
     private FlightModel flightModel;
     
+    @Inject
+    private FlightSession flightSession;
+    
     private Long id;
-
+ 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -54,5 +61,38 @@ public class ViewOrderBB implements Serializable {
             return null;
         List<Passenger> ps = flightModel.getPassengerList().getByOrder(o);
         return ps;
+    }
+    
+    public boolean notDepartured() {
+        if(id == null)
+            return false;
+        Order o = flightModel.getOrderList().find(id);
+        Calendar departure = o.getOrderFlight().getDeparture();
+        Calendar current = Calendar.getInstance();
+        return departure.after(current);
+    }
+    
+    public boolean updatableBaggage(String baggage) {
+        User u = flightSession.getUser();
+        Order o = flightModel.getOrderList().find(id);
+        if(!o.getOrderUser().equals(u))
+            return false;
+        if((baggage.equals("Handbag") || baggage.equals("Small")) && notDepartured())
+            return true;
+        return false;
+    }
+    
+    public boolean viewCheck() {
+        if(id == null)
+            return false;
+        Order o = flightModel.getOrderList().find(id);
+        if(o == null)
+            return false;
+        User u = flightSession.getUser();
+        if (u.getGroups().get(0).equals(Groups.ADMIN))
+            return true;
+        if(o.getOrderUser().getId() == u.getId())
+            return true;
+        return false;
     }
 }
